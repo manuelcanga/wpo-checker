b5a4bc2fb7fe9442dfa8beeed25b5fbbba6f775b
Manuel Canga <dev@trasweb.net>
2020-02-08 00:54:55 +0100

    feat: Add description to sites

--- src/Entities/class-site.php
+++ src/Entities/class-site.php
@@ -7,30 +7,30 @@ namespace Trasweb\Plugins\WpoChecker\Entities;
  *
  * @package Entities
  */
-final class Site
-{
-    public $id;
-    public $name;
-    public $short_name;
-    public $link;
-    public $method;
-    public $input_name;
-    public $active;
+final class Site {
+	public $id;
+	public $name;
+	public $short_name;
+	public $link;
+	public $method;
+	public $input_name;
+	public $active;
+	public $description;
 
-    /**
-     * Site constructor.
-     *
-     * @param string $current_site_id
-     * @param array  $current_site_options
-     */
-    final public function __construct(string $current_site_id, array $current_site_options)
-    {
-        $this->id         = $current_site_id;
-        $this->name       = $current_site_options['name'];
-        $this->short_name = $current_site_options['short_name'] ?? $this->name;
-        $this->link       = $current_site_options['link'];
-        $this->method     = $current_site_options['method'] ?? 'post';
-        $this->input_name = $current_site_options['var'] ?? 'url';
-        $this->active     = $current_site_options['active'] ?? true;
-    }
+	/**
+	 * Site constructor.
+	 *
+	 * @param string $current_site_id
+	 * @param array  $current_site_options
+	 */
+	final public function __construct( string $current_site_id, array $current_site_options ) {
+		$this->id          = $current_site_id;
+		$this->name        = $current_site_options['name'];
+		$this->short_name  = $current_site_options['short_name'] ?? $this->name;
+		$this->link        = $current_site_options['link'];
+		$this->method      = $current_site_options['method'] ?? 'post';
+		$this->input_name  = $current_site_options['var'] ?? 'url';
+		$this->active      = $current_site_options['active'] ?? true;
+		$this->description = $current_site_options['description'] ?? '';
+	}
 }
