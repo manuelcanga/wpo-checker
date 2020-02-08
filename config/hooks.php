1ec55c3aeaadcb579d8f36bf2403d03a27b6a323
Manuel Canga <dev@trasweb.net>
2020-02-08 13:56:57 +0100

    feature: Add redirecto to config page when after plugin activation

--- config/hooks.php
+++ config/hooks.php
@@ -1,4 +1,5 @@
 <?php
+declare( strict_types = 1 );
 
 namespace Trasweb\Plugins\WpoChecker;
 
@@ -11,50 +12,54 @@ use Trasweb\Plugins\WpoChecker\Repositories\Settings;
 use Trasweb\Plugins\WpoChecker\Repositories\Sites;
 
 return [
-    Hook::new('post_row_actions', [
-        'listener' => Posts_List::class,
-        'filter'   => 'show_wpo_links_in_cpt_row_actions',
-    ]),
-    Hook::new('page_row_actions', [
-        'listener' => Posts_List::class,
-        'filter'   => 'show_wpo_links_in_cpt_row_actions',
-    ]),
-    Hook::new('tag_row_actions', [
-        'listener' => Terms_List::class,
-        'filter'   => 'show_wpo_links_in_tags_row_actions',
-    ]),
-    Hook::new('admin_menu', [
-        'listener' => Sites_Selection::class,
-        'action'   => 'show_menu',
-        'options'  => [
-            'page_title' => __('Sites selection', PLUGIN_NAME),
-            'menu_title' => PLUGIN_TITLE,
-            'capability' => 'manage_options',
-            'menu_slug'  => 'sites_selection_page',
-        ],
-    ]),
-    Hook::new('plugin_action_links', [
-        'listener' => Plugins_List::class,
-        'filter'   => 'show_settings_in_plugin_links',
-        'options'  => [
-            'link_to_settings' => [
-                'capability' => 'manage_options',
-                'slug'       => 'sites_selection_page',
-                'label'      => __('Settings', PLUGIN_NAME),
-                'format'     => '<a href="%s">%s</a>',
-            ],
-        ],
-    ]),
-    Hook::new(PLUGIN_NAME . '-loaded', [
-        'listener'  => Settings::class,
-        'action'    => 'register',
-        'with_args' => false,
-        'options'   => [
-            'sites' => [
-                'sanitize_callback' => Hook::callback(Sites::class, 'sanitize'),
-                'show_in_rest'      => false,
-                'default'           => [],
-            ],
-        ],
-    ]),
+	Hook::new( 'post_row_actions', [
+		'listener' => Posts_List::class,
+		'filter'   => 'show_wpo_links_in_cpt_row_actions',
+	] ),
+	Hook::new( 'page_row_actions', [
+		'listener' => Posts_List::class,
+		'filter'   => 'show_wpo_links_in_cpt_row_actions',
+	] ),
+	Hook::new( 'tag_row_actions', [
+		'listener' => Terms_List::class,
+		'filter'   => 'show_wpo_links_in_tags_row_actions',
+	] ),
+	Hook::new( 'admin_menu', [
+		'listener' => Sites_Selection::class,
+		'action'   => 'show_menu',
+		'options'  => [
+			'page_title' => __( 'Sites selection', PLUGIN_NAME ),
+			'menu_title' => PLUGIN_TITLE,
+			'capability' => 'manage_options',
+			'menu_slug'  => Plugin::CONFIG_PAGE,
+		],
+	] ),
+	Hook::new( 'plugin_action_links', [
+		'listener' => Plugins_List::class,
+		'filter'   => 'show_settings_in_plugin_links',
+		'options'  => [
+			'link_to_settings' => [
+				'capability' => 'manage_options',
+				'slug'       => Plugin::CONFIG_PAGE,
+				'label'      => __( 'Settings', PLUGIN_NAME ),
+				'format'     => '<a href="%s">%s</a>',
+			],
+		],
+	] ),
+	Hook::new( 'wp_redirect', [
+		'listener' => Plugins_List::class,
+		'filter'   => 'redirect_to_sites_selection',
+	] ),
+	Hook::new( PLUGIN_NAME . '-loaded', [
+		'listener'  => Settings::class,
+		'action'    => 'register',
+		'with_args' => false,
+		'options'   => [
+			'sites' => [
+				'sanitize_callback' => Hook::callback( Sites::class, 'sanitize' ),
+				'show_in_rest'      => false,
+				'default'           => [],
+			],
+		],
+	] ),
 ];
--- src/Places/class-plugins-list.php
+++ src/Places/class-plugins-list.php
@@ -1,7 +1,10 @@
 <?php
+declare( strict_types = 1 );
 
 namespace Trasweb\Plugins\WpoChecker\Places;
 
+use Trasweb\Plugins\WpoChecker\Plugin;
+use function admin_url;
 use const Trasweb\Plugins\WpoChecker\PLUGIN_NAME;
 
 /**
@@ -9,53 +12,83 @@ use const Trasweb\Plugins\WpoChecker\PLUGIN_NAME;
  *
  * @package Pages
  */
-class Plugins_List
-{
-    /**
-     * Links to settings data
-     *
-     * @var array{capability; string, slug: string, label: string, format: string}
-     */
-    private $link_to_settings;
+class Plugins_List {
+	private const PLUGIN_INIT_FILE = PLUGIN_NAME . '/' . PLUGIN_NAME . '.php';
+	/**
+	 * Links to settings data
+	 *
+	 * @var array{capability; string, slug: string, label: string, format: string}
+	 */
+	private $link_to_settings;
 
-    /**
-     * Plugins_List constructor.
-     *
-     * @param array<string, array> $config
-     */
-    public function __construct(array $config)
-    {
-        $this->link_to_settings = $config['link_to_settings'];
-    }
+	/**
+	 * Plugins_List constructor.
+	 *
+	 * @param array<string, array> $config
+	 */
+	public function __construct( array $config = [] )
+	{
+		$this->link_to_settings = $config[ 'link_to_settings' ] ?? [];
+	}
 
-    /**
-     * Enqueue link to settings page in plugins list page.
-     *
-     * @param $links
-     * @param $file
-     *
-     * @return array
-     */
-    public function show_settings_in_plugin_links(array $links, string $file): array
-    {
-        $init_file = PLUGIN_NAME.'/'.PLUGIN_NAME.".php";
+	/***
+	 * Redirect to config page when plugin is activated.
+	 *
+	 * @param string $url Default url of filter.
+	 *
+	 * @filter wp_redirect
+	 *
+	 * @return string Default url or url to plugin settings page
+	 */
+	public function redirect_to_sites_selection( string $url ): string
+	{
+		$redirect_normally = $url;
 
-        if ($file === $init_file && current_user_can($this->link_to_settings['capability'])) {
-            array_unshift($links, $this->get_settings_link());
-        }
+		if ( ! defined( Plugin::NAMESPACE . '\PLUGIN_ACTIVATION' ) ) {
+			return $redirect_normally;
+		}
 
-        return $links;
-    }
+		$is_final_redirection = strpos( $url, admin_url( 'plugins.php?activate=true' ) ) !== false;
+		if ( ! $is_final_redirection ) {
+			return $redirect_normally;
+		}
 
-    /**
-     * Retrieve a link to settings page of plugin.
-     *
-     * @return string
-     */
-    private function get_settings_link(): string
-    {
-        $settings_url = admin_url('options-general.php?page=' . $this->link_to_settings['slug']);
+		$is_activation_bulk = $_REQUEST[ 'activate-multi' ] ?? false;
+		if ( $is_activation_bulk ) {
+			return $redirect_normally;
+		}
 
-        return sprintf($this->link_to_settings['format'], $settings_url, $this->link_to_settings['label']);
-    }
+		$sites_selection_url = add_query_arg( [ 'page' => Plugin::CONFIG_PAGE ], admin_url( 'admin.php' ) );
+
+		return $redirect_to = $sites_selection_url;
+	}
+
+	/**
+	 * Enqueue link to settings page in plugins list page.
+	 *
+	 * @param $links
+	 * @param $file
+	 *
+	 * @return array
+	 */
+	public function show_settings_in_plugin_links( array $links, string $file ): array
+	{
+		if ( $file === self::PLUGIN_INIT_FILE && current_user_can( $this->link_to_settings[ 'capability' ] ) ) {
+			array_unshift( $links, $this->get_settings_link() );
+		}
+
+		return $links;
+	}
+
+	/**
+	 * Retrieve a link to settings page of plugin.
+	 *
+	 * @return string
+	 */
+	private function get_settings_link(): string
+	{
+		$settings_url = admin_url( 'options-general.php?page=' . $this->link_to_settings[ 'slug' ] );
+
+		return sprintf( $this->link_to_settings[ 'format' ], $settings_url, $this->link_to_settings[ 'label' ] );
+	}
 }
