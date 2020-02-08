3bbf557fa54bac5787694d48d22733af1dd2ad56
Manuel Canga <dev@trasweb.net>
2020-02-08 17:59:35 +0100

    tests: Add test in order to check if sites in config page are the same to sites in config file.

--- tests/Tests/acceptance/SettingsPageCest.php
+++ tests/Tests/acceptance/SettingsPageCest.php
@@ -3,6 +3,7 @@
 namespace Tests\acceptance;
 
 use Tests\Step\Acceptance\WpAdmin;
+use Trasweb\Plugins\WpoChecker\Repositories\Sites;
 
 class SettingsPageCest {
     public function exists(WpAdmin $I) {
@@ -11,4 +12,15 @@ class SettingsPageCest {
 		$I->openMenu('Ajustes');
 		$I->openMenu( 'WPO Checker');
     }
+
+	public function containSites(WpAdmin $I) {
+		$I->wantToTest( 'I can see "WPO Checker" settings page');
+		$I->loginAsUser( );
+		$I->openMenu('Ajustes');
+		$I->openMenu( 'WPO Checker');
+		$actual_sites = $I->grabMultiple( '#the-list tr');
+		$expected_sites = (new Sites() )->get_available_site_collection();
+
+		$I->assertSame( count($expected_sites), count($actual_sites));
+	}
 }
